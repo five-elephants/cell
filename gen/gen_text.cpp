0948a07390335ef310a37ce61550664171548953
Simon Friedmann <simonf256@googlemail.com>
2012-09-14 23:15:03 +0200

    gitignore, missing files

---
+++ gen/gen_text.cpp
@@ -0,0 +1,122 @@
+#include "gen/gen_text.h"
+
+#include "ast/ast.h"
+
+namespace gen {
+
+	void
+	Text_generator::op_plus(ast::Op_plus& op) {
+		m_out << '(';
+	 	op.left().visit();
+	 	m_out	<< " + ";
+	 	op.right().visit();
+		m_out	<< ')';
+	}
+
+	void
+	Text_generator::op_minus(ast::Op_minus& op) {
+		m_out << '(';
+	 	op.left().visit();
+	 	m_out	<< " - ";
+	 	op.right().visit();
+		m_out	<< ')';
+	}
+
+	void
+	Text_generator::op_mult(ast::Op_mult& op) {
+		m_out << '(';
+		op.left().visit();
+		m_out << " * ";
+		op.right().visit();
+		m_out << ')';
+	}
+
+	void
+	Text_generator::op_div(ast::Op_div& op) {
+		m_out << '(';
+		op.left().visit();
+		m_out << " / ";
+		op.right().visit();
+		m_out << ')';
+	}
+		
+
+	void
+	Text_generator::int_literal(ast::Literal<int>& lit) {
+		m_out << lit.value();
+	}
+
+
+	void
+	Text_generator::var_assign(ast::Var_assign& a) {
+		indent();
+		m_out << "(var ";
+		a.identifier().visit();
+		m_out << " : ";
+		a.type().visit();
+		m_out << " = ";
+		a.expression().visit();
+		m_out << ")\n";
+	}
+
+
+	void
+	Text_generator::identifier(ast::Identifier& id) {
+		m_out << id.identifier();
+	}
+
+
+	void
+	Text_generator::module_begin(ast::Module_def& mod) {
+		indent();
+		m_out << "(mod ";
+		mod.identifier().visit();
+		m_out << " (\n";
+		++m_indent;
+	}
+
+
+	void
+	Text_generator::module_end(ast::Module_def& mod) {
+		--m_indent;
+		indent();
+		m_out << ")\n";
+	}
+
+
+	void
+	Text_generator::function_begin(ast::Function_def& f) {
+		indent();
+		m_out << "(function ";
+		f.identifier().visit();
+		m_out << " (parameters (\n";
+		++m_indent;
+	}
+
+
+	void
+	Text_generator::function_body(ast::Function_def& f) {
+		--m_indent;
+		indent();
+		m_out << ")) (body (\n";
+		++m_indent;
+	}
+
+
+	void
+	Text_generator::function_end(ast::Function_def& f) {
+		--m_indent;
+		indent();
+		m_out << "))\n";
+	}
+
+
+	void
+	Text_generator::indent() const {
+		for(int i=0; i<m_indent; i++) {
+			m_out << "  ";
+		}
+	}
+
+
+}
---
+++ gen/gen_text.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "gen/generator_if.h"
+#include <ostream>
+
+namespace gen {
+
+	class Text_generator : public Generator_if {
+		public:
+			Text_generator(std::ostream& out)
+				:	m_out(out) {
+					m_indent = 0;
+			}
+			virtual ~Text_generator() {};
+
+			virtual void op_plus(ast::Op_plus& op);
+			virtual void op_minus(ast::Op_minus& op);
+			virtual void op_mult(ast::Op_mult& op);
+			virtual void op_div(ast::Op_div& op);
+			virtual void int_literal(ast::Literal<int>& lit);
+			virtual void var_assign(ast::Var_assign& a);
+			virtual void identifier(ast::Identifier& id);
+			virtual void module_begin(ast::Module_def& mod);
+			virtual void module_end(ast::Module_def& mod);
+			virtual void function_begin(ast::Function_def& f);
+			virtual void function_body(ast::Function_def& f);
+			virtual void function_end(ast::Function_def& f);
+
+
+		private:
+			std::ostream& m_out;
+			int m_indent;
+
+			void indent() const;
+	};
+
+}
