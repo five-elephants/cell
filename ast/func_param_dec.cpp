b5a0880d902fd2f5fefef152a4833a18bfb89fcf
Simon Friedmann <simonf256@googlemail.com>
2013-05-17 12:49:23 +0200

    func_param_dec.h/cpp ?

---
+++ ast/func_param_dec.cpp
@@ -0,0 +1,25 @@
+#include "ast/func_param_dec.h"
+
+namespace ast {
+
+	Func_param_dec::Func_param_dec(Node_if& identifier)
+		:	Variable_def(identifier) {
+	}
+
+	Func_param_dec::Func_param_dec(Node_if& identifier,
+			Node_if& type,
+			Node_if& expression)
+		:	
+			Variable_def(identifier, type, expression) {
+	}
+
+	Func_param_dec::~Func_param_dec() {
+	}
+
+	void
+	Func_param_dec::visit() {
+		get_generator().func_param_dec(*this);
+	}
+
+}
+
---
+++ ast/func_param_dec.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ast/variable_def.h"
+
+namespace ast {
+
+	class Func_param_dec : public Variable_def {
+		public:
+			Func_param_dec(Node_if& identifier);
+			Func_param_dec(Node_if& identifier,
+					Node_if& type,
+					Node_if& expression);
+			virtual ~Func_param_dec();
+
+			virtual void set_generator(gen::Generator_if& g); 
+			virtual void visit();
+	};
+
+}
+
