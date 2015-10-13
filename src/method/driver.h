95ac6c48c502382aec17c0d960b6cacab5060e2a
Simon Friedmann <simonf256@googlemail.com>
2015-10-13 23:23:08 +0200

    Add documentation for method classes

--- src/method/driver.h
+++ src/method/driver.h
@@ -3,15 +3,38 @@
 #include "logging/logger.h"
 #include "sim/simulation_engine.h"
 
+/** Verification methodology */
 namespace method {
 
-  template<typename Base, typename Socket>
+  /** Interface to execution of simulation.
+   *
+   * @tparam Derived The derived class for CRTP.
+   * @tparam Socket Type for the socket of the targetted module. Generate with
+   * cellwrap.
+   *
+   * Specialize this class to your needs and use it together with
+   * Simulation_engine::add_driver() to register callbacks for a module during
+   * the simulation cycle. In every cycle i.e. everytime processes are run
+   * within this module Driver will call observe and drive function of the
+   * derived class Derived. The expected function signatures are
+   *
+   * - void Derived::observe(ir::Time const& t, Socket port)
+   * - Socket Derived::drive(ir::Time const& t, Socket port)
+   *
+   * In both cases port will be loaded with the contents of the this_in module
+   * frame.  For drive the return value will overwrite this_out at the beginnig
+   * of the cycle.
+   * */
+  template<typename Derived, typename Socket>
   class Driver {
     public:
       Driver()
         : m_logger(log4cxx::Logger::getLogger("tb.Driver")) {
       }
 
+      /** Call back from sim::Simulation_engine.
+       *
+       * Converts raw sim::Runset::Module_frame to type Socket. */
       void operator () (ir::Time const& t,
           sim::Runset::Module_frame this_in,
           sim::Runset::Module_frame this_out,
@@ -20,20 +43,27 @@ namespace method {
         Socket s = from_frame(this_in);
         LOG4CXX_TRACE(m_logger, "@" << t << " in : " << s);
 
-        static_cast<Base*>(this)->observe(t, s);
-        s = static_cast<Base*>(this)->drive(t, s);
+        static_cast<Derived*>(this)->observe(t, s);
+        s = static_cast<Derived*>(this)->drive(t, s);
 
         LOG4CXX_TRACE(m_logger, "@" << t << " out: " << s);
         to_frame(s, this_out);
       }
 
     protected:
+      /** Convert from sim::Runset::Module_frame to type Socket.
+       *
+       * @param frame Module frame to convert. */
       Socket from_frame(sim::Runset::Module_frame frame) const {
         Socket rv;
         std::copy_n(frame->data(), sizeof(Socket), reinterpret_cast<uint8_t*>(&rv));
         return rv;
       }
 
+      /** Convert to sim::Runset::Module_frame from type Socket.
+       *
+       * @param socket Source of conversion.
+       * @param frame Target of conversion. */
       void to_frame(Socket socket, sim::Runset::Module_frame frame) const {
         std::copy_n(reinterpret_cast<uint8_t*>(&socket), sizeof(Socket), frame->data());
       }
--- src/method/observer_channel.h
+++ src/method/observer_channel.h
@@ -9,21 +9,44 @@
 
 namespace method {
 
-  template<typename Base, typename Socket>
-  class Observer_channel : public Driver<Observer_channel<Base, Socket>, Socket> {
+  /** Clocked channel to observe a module during simulation.
+   *
+   * @tparam Derived Derived class for CRTP.
+   * @tparam Socket Type of socket of targetted module. Generate with cellwrap.
+   *
+   * Specialize this class to observe modules during simulation in a synchronized fashion.
+   * The derived class needs to implement a clocked() and a cycle() function
+   * that get called to detect clock events and process data.
+   *
+   * - bool clocked(Socket in, Socket prev)
+   * - void cycle(Socket port)
+   *
+   * Observer_channel launches a thread and passes data from the simulation
+   * thread through a boost::lockfree::spsc_queue. For each data item the
+   * cycle() function is called individually in the observer's thread.
+   * */
+  template<typename Derived, typename Socket>
+  class Observer_channel : public Driver<Observer_channel<Derived, Socket>, Socket> {
     public:
+      /** Constructor.
+       *
+       * Start a new thread for observation using the run() member. */
       Observer_channel()
-        : Driver<Observer_channel<Base, Socket>, Socket>(),
+        : Driver<Observer_channel<Derived, Socket>, Socket>(),
           m_stop(false),
-          m_thread(&Observer_channel<Base,Socket>::run,
-              static_cast<Base*>(this)) {
+          m_thread(&Observer_channel<Derived,Socket>::run,
+              static_cast<Derived*>(this)) {
       }
 
+      /** Destructor.
+       *
+       * Signal a stop request to the thread and wait for it to finish. */
       ~Observer_channel() {
         m_stop = true;
         m_thread.join();
       }
 
+      /** Call back for sim::Simulation_engine. */
       void operator () (ir::Time const& t,
           sim::Runset::Module_frame this_in,
           sim::Runset::Module_frame this_out,
@@ -31,13 +54,18 @@ namespace method {
         Socket in = this->from_frame(this_in);
         Socket prev = this->from_frame(this_prev);
 
-        if( static_cast<Base*>(this)->clocked(in, prev) ) {
+        if( static_cast<Derived*>(this)->clocked(in, prev) ) {
           LOG4CXX_TRACE(this->m_logger, "in : " << in << " -> push");
           while( !m_queue.push(in) )
             ;
         }
       }
 
+      /** Entry point for the observer's thread.
+       *
+       * Polls the queue and calls Derived::cycle() for each data item.  Also
+       * monitors m_stop to exit the thread on request.
+       * */
       void run() {
         LOG4CXX_DEBUG(this->m_logger, "Starting observer thread");
 
@@ -48,13 +76,18 @@ namespace method {
             ;
 
           LOG4CXX_TRACE(this->m_logger, "observing: " << sock);
-          static_cast<Base*>(this)->cycle(sock);
+          static_cast<Derived*>(this)->cycle(sock);
         }
       }
 
     private:
+      /** Queue for inter-thread communication. */
       boost::lockfree::spsc_queue<Socket, boost::lockfree::capacity<64>> m_queue;
+
+      /** Flag for requesting the thread to stop. */
       std::atomic_bool m_stop;
+
+      /** Thread object. */
       std::thread m_thread;
   };
 
